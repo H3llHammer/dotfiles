f106cab3a5daaee7bd6ab120999a1471ca75af71
Alberto T. Fuentes Ramirez <albertofuentes505@outlook.com>
2021-07-01 16:01:57 -0500

    changed from Key to EzKey

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -4,67 +4,69 @@ import subprocess
 from typing import List  # noqa: F401
 
 from libqtile import bar, layout, widget, hook
-from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.config import Click, Drag, Group, EzKey, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
 mod = "mod4"
 terminal = guess_terminal()
 
+#modifier_keys = {
+#   'M': 'mod4',
+#   'A': 'mod1',
+#   'S': 'shift',
+#   'C': 'control',
+#}
+
 keys = [
     # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(),
-        desc="Move window focus to other window"),
+    EzKey("M-h", lazy.layout.left(), desc="Move focus to left"),
+    EzKey("M-l", lazy.layout.right(), desc="Move focus to right"),
+    EzKey("M-j", lazy.layout.down(), desc="Move focus down"),
+    EzKey("M-k", lazy.layout.up(), desc="Move focus up"),
+    EzKey("M-<space>", lazy.layout.next(), desc="Move window focus to other window"),
 
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
-        desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
-        desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
-        desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    EzKey("M-S-h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    EzKey("M-S-l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    EzKey("M-S-j", lazy.layout.shuffle_down(), desc="Move window down"),
+    EzKey("M-S-k", lazy.layout.shuffle_up(), desc="Move window up"),
 
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(),
-        desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(),
-        desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(),
-        desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    EzKey("M-C-h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    EzKey("M-C-l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    EzKey("M-C-j", lazy.layout.grow_down(), desc="Grow window down"),
+    EzKey("M-C-k", lazy.layout.grow_up(), desc="Grow window up"),
+    EzKey("M-n", lazy.layout.normalize(), desc="Reset all window sizes"),
 
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
+    EzKey("M-S-<Return>", lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack"),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    EzKey("M-<Return>", lazy.spawn(terminal), desc="Launch terminal"),
 
     # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Put the focused window to/from fullscreen mode"),
-    Key([mod, "shift"], "f", lazy.window.toggle_floating(), desc="Put the focused window to/from floating mode"),
+    EzKey("M-<Tab>", lazy.next_layout(), desc="Toggle between layouts"),
+    EzKey("M-w", lazy.window.kill(), desc="Kill focused window"),
+    EzKey("M-f", lazy.window.toggle_fullscreen(),
+        desc="Put the focused window to/from fullscreen mode"),
+    EzKey("M-S-f", lazy.window.toggle_floating(),
+        desc="Put the focused window to/from floating mode"),
 
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(),
-        desc="Spawn a command using a prompt widget"),
+    EzKey("M-C-r", lazy.restart(), desc="Restart Qtile"),
+    EzKey("M-C-q", lazy.shutdown(), desc="Shutdown Qtile"),
+    EzKey("M-r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 
     # Hotkeys
-    Key([mod], "d", lazy.spawn("rofi -show drun -show-icons -icon-theme Papirus")),
-    Key([mod], "e", lazy.spawn(terminal + " -e nnn")),
-    Key([mod, "shift"], "s", lazy.spawn("flameshot gui")),
-    Key([mod], "p", lazy.spawn("pavucontrol")),
+    EzKey("M-d", lazy.spawn("rofi -show drun -show-icons -icon-theme Papirus")),
+    EzKey("M-S-e", lazy.spawn(terminal + " -e nnn")),
+    EzKey("M-e", lazy.spawn("pcmanfm")),
+    EzKey("M-S-s", lazy.spawn("flameshot gui")),
+    EzKey("M-p", lazy.spawn("pavucontrol")),
 ]
 
 colors = [["#383E56"], # panel background
@@ -93,25 +95,6 @@ layouts = [
     layout.MonadTall(border_width=1,margin=5),
     layout.Columns(border_focus_stack='#d75f5f'),
     layout.Max(),
-    layout.TreeTab(
-         fontsize = 10,
-         sections = ["FIRST", "SECOND", "THIRD", "FOURTH"],
-         section_fontsize = 10,
-         border_width = 2,
-         bg_color = "1c1f24",
-         active_bg = "c678dd",
-         active_fg = "000000",
-         inactive_bg = "a9a1e1",
-         inactive_fg = "1c1f24",
-         padding_left = 0,
-         padding_x = 0,
-         padding_y = 5,
-         section_top = 10,
-         section_bottom = 20,
-         level_shift = 8,
-         vspace = 3,
-         panel_width = 200
-         )
 ]
 
 widget_defaults = dict(
@@ -154,7 +137,6 @@ screens = [
                 widget.Prompt(),
                 widget.Sep(
                        linewidth = 0,
-                       padding = 6,
                        foreground = colors[2],
                        background = colors[0]
                        ),
@@ -185,16 +167,11 @@ screens = [
                     step=5,
                     ),
                 widget.Clock(
-                    fontsize=14,
                     foreground='5AA469',
                     format=' %a %d %b %I:%M %p',
                     ),
-                widget.CurrentLayout(
-                    background='776D8A'
-                        ),
-                widget.QuickExit(
-                    foreground='CD5D7D'
-                        ),
+                widget.CurrentLayout(background='776D8A'),
+                widget.QuickExit(foreground='CD5D7D'),
             ],
             24,
         ),
@@ -223,9 +200,12 @@ floating_layout = layout.Floating(float_rules=[
     Match(wm_class='maketag'),  # gitk
     Match(wm_class='ssh-askpass'),  # ssh-askpass
     Match(wm_class='Pavucontrol'), # volume control
+    Match(wm_class='Nm-connection-editor'), # volume control
+    Match(wm_class='Lxappearance'), # volume control
     Match(title='branchdialog'),  # gitk
     Match(title='pinentry'),  # GPG key password entry
 ])
+
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
