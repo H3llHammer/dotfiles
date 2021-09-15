a6b7b302c72d11ff1a89cb56a7d326fd5b6ee0ff
Alberto Fuentes <albertofuentes505@outlook.com>
2021-09-14 21:19:24 -0500

    dwm custom status bar

---
+++ Scripts/dwm-bar.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+
+vol()
+{
+    echo $(amixer get Master | awk ' /Front Left:/ { if ( $6 == "[on]" ) print " " $5; else print "婢 " $6 }' | tr -d "[]")
+}
+
+mic()
+{
+    echo $(amixer get Capture | awk '/Front Left:/ { if ( $6 == "[on]" ) print " " $5; else print " " $6 }' | tr -d "[]")
+}
+
+kern()
+{
+    echo $(uname -r)
+}
+
+mem()
+{
+    echo $(free -h | awk '/Mem:/ { print $3 "/"$2 }')
+    sleep 10
+}
+
+dat()
+{
+    echo $(date "+%A %d %G  %H:%M")
+}
+
+while :
+do
+    xsetroot -name " $(vol) | $(mic) |  $(kern) |  $(dat) "
+    sleep 1
+done
