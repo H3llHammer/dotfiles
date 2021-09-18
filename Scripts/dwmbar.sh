3ebc4beaff42e0a62c319d21053011cf5aa36493
Alberto Fuentes <albertofuentes505@outlook.com>
2021-09-18 12:52:22 -0500

    added ansi color variables and a function to display used and total ram memory

--- Scripts/dwmbar.sh
+++ Scripts/dwmbar.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-RED='\033[0;31m'
+# Ansi color code variables
+red="\e[0;91m"
+blue="\e[0;94m"
+expand_bg="\e[K"
+blue_bg="\e[0;104m${expand_bg}"
+red_bg="\e[0;101m${expand_bg}"
+green_bg="\e[0;102m${expand_bg}"
+green="\e[0;92m"
+white="\e[0;97m"
+bold="\e[1m"
+uline="\e[4m"
+reset="\e[0m"
 
 vol()
 {
@@ -19,8 +30,7 @@ kern()
 
 mem()
 {
-    echo $(free -h | awk '/Mem:/ { print $3 "/"$2 }')
-    sleep 10
+    echo$(free -h | awk '/Mem/ { print "Used: " $3 " total: " $2 }')
 }
 
 dat()
