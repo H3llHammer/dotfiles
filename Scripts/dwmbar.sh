b6f8f1f15ce4ddeed93060efaa1b59592ab5ae22
Alberto Fuentes <albertofuentes505@outlook.com>
2021-09-27 22:27:59 -0500

    removed console colors

--- Scripts/dwmbar.sh
+++ Scripts/dwmbar.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Ansi color code variables
-red="\e[0;91m"
-blue="\e[0;94m"
-expand_bg="\e[K"
-blue_bg="\e[0;104m${expand_bg}"
-red_bg="\e[0;101m${expand_bg}"
-green_bg="\e[0;102m${expand_bg}"
-green="\e[0;92m"
-white="\e[0;97m"
-bold="\e[1m"
-uline="\e[4m"
-reset="\e[0m"
-
 vol()
 {
     echo $(amixer get Master | awk ' /Front Left:/ { if ( $6 == "[on]" ) print " " $5; else print "婢 " $6 }' | tr -d "[]")
