3835122c80417fe8a9c749543665495c06eab82f
Alberto Fuentes <albertofuentes505@outlook.com>
2022-03-17 16:00:27 -0600

    the colors were moved to an individual file

---
+++ .config/qtile/settings/colors.py
@@ -0,0 +1,13 @@
+colors = [["282C34"],  # 0 panel background
+          ["3d3f4b"],  # 1 background for current screen tab
+          ["ffffff"],  # 2 font color for group names (White)
+          ["FF7171"],  # 3 border line color for current tab
+          ["74438f"],  # 4 border line color for 'other tabs' and color for 'odd widgets'
+          ["6886C5"],  # 5 color for the 'even widgets'
+          ["ff5131"],  # 6 window name
+          ["92967D"],  # 7 foreground for inactive screens
+          ["32E0C4"],  # 8 Mint
+          ["DDDDDD"],  # 9 white variant
+          ["3DB2FF"],  # 10 Blue arch linux
+          ["FF4848"],  # 11 Red
+          ["F0A500"]]  # 12 Orange
