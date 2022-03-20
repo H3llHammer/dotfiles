451a8a3a45bd64873a73a77d31d64f5c8366b5f8
Alberto Fuentes <albertofuentes505@outlook.com>
2022-03-20 11:37:10 -0600

    changed border focus color

--- .config/qtile/settings/layouts.py
+++ .config/qtile/settings/layouts.py
@@ -1,10 +1,10 @@
 from libqtile import layout
-from settings.colors import one_dark
+from settings.colors import Colors
 
 layouts = [
-    layout.MonadTall(border_focus=one_dark[1], border_width=1, margin=10),
-    layout.MonadWide(bborder_focus=one_dark[1], order_width=1, margin=10),
-    layout.Matrix(border_focus=one_dark[1], order_width=1, margin=10),
+    layout.MonadTall(border_focus=Colors["Red"], border_width=1, margin=10),
+    layout.MonadWide(bborder_focus=Colors["Red"], order_width=1, margin=10),
+    layout.Matrix(border_focus=Colors["Red"], order_width=1, margin=10),
     layout.Max(),
     layout.Floating(),
 ]
